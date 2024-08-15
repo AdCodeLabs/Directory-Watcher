fab2767da815039998cf4ad1b8337665ef83f139
23vugarr <vabdulaliyev@gmail.com>
2024-08-15 16:27:23 +0400

    local watcher beta version

--- cmd/directoryWatcher/main.go
+++ cmd/directoryWatcher/main.go
@@ -4,6 +4,8 @@ import (
 	watcher2 "directoryWatcher/pkg/watcher"
 	"flag"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -18,5 +20,21 @@ func main() {
 		log.Fatalf("error while initializing the watcher...")
 	}
 
-	watcher.Start(*dirPath, *serverIp)
+	dirsToWatch := make([]string, 0)
+
+	err = filepath.Walk(*dirPath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				dirsToWatch = append(dirsToWatch, path)
+			}
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+
+	watcher.Start(*serverIp, dirsToWatch)
 }
